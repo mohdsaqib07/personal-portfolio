b814f83892752af7106cb0b10c13412dfb4ead4b
Mohd Saqib <mohdsaqib148183@gmail.com>
2026-03-29 18:04:05 +0530

    change the node version to the old one

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -15,10 +15,10 @@
 <header class={"sticky z-[10] top-0 duration-200 px-6 flex items-center justify-between border-solid " + (
     y>0 ?'py-4 bg-slate-950 border-violet-950' : 'py-6 bg-transparent border-transparent'
 )}>
-    <h1 class="font-medium">
+    <h1 class="font-medium capitalize">
         <b class="font-bold poppins">Mohd</b> Saqib
     </h1>
-        <div class="sm:flex ml-auto pr-4 items-center gap-4 hidden">
+        <div class="items-center hidden gap-4 pr-4 ml-auto sm:flex">
             {#each tabs as tab,index }
                 <a href={tab.link} class="duration-200 hover:text-violet-400">
                     <p>{tab.name}</p>
@@ -26,8 +26,8 @@
             {/each}
             
         </div>
-        <a href="https://www.linkedin.com/in/mohd-saqib-142a68250/" target="_blank" class="bluShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950">
-            <div class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"></div>
+        <a href="https://www.linkedin.com/in/mohd-saqib-142a68250/" target="_blank" class="relative px-5 py-2 overflow-hidden bg-white rounded-full bluShadow group text-slate-950">
+            <div class="absolute top-0 z-0 w-full h-full duration-200 right-full bg-violet-400 opacity-20 group-hover:translate-x-full"></div>
             <h4 class="relative z-[9]">Get in Touch</h4>
         </a>
 </header>
